5c9e970a2d472d0040a35bc304f905a280983c6d
Cesar Rodas <rodascesar03@yahoo.com>
2017-07-09 23:53:56 -0400

    working teacher

--- controller.js
+++ controller.js
@@ -1,10 +1,31 @@
 const { ipcRenderer } = require('electron');
 
-let page = 0;
+let page = 1;
 
-document.getElementById('instruct').innerHTML = "<h1>cesar</h1>";
+
+function render(page) {
+  img = `<img class="instruction" src="./images/flexbox_${page}.png" />`;
+  document.getElementById('instruct').innerHTML = img;
+}
+
+document.getElementById('back').onclick = function lastPage () {
+  if(page > 1){
+    page = page - 1;
+    render(page);
+  }
+};
+
+document.getElementById('next').onclick = function nextPage () {
+  if(page < 10){
+    page = page + 1;
+    render(page);
+  }
+}
 
 ipcRenderer.on('page', (event, props) => {
-  img = `<img class="instruction" src="./images/flexbox_${props.count}.png" />`;
-  document.getElementById('instruct').innerHTML = img;
+  //img = `<img class="instruction" src="./images/flexbox_${props.count}.png" />`;
+  //document.getElementById('instruct').innerHTML = img;
+  render(props.count);
 });
+
+render(1);
